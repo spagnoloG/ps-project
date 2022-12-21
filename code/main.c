0e27864e3aaa69a1ea56fdb9abb061adf49ba485
Spagnolo Gasper <gasper.spagnolo@outlook.com>
2022-12-21 12:12:02 +0100

    Update makefile and initial code

--- code/main.c
+++ code/main.c
@@ -6,58 +6,95 @@
 #define DT 0.001  // time step
 #define MAX_ITERATIONS 10000  // maximum number of iterations
 
-// structure to represent a celestial body
 typedef struct {
-  double mass;  // mass of the body
-  double x;  // x-coordinate of the body's position
-  double y;  // y-coordinate of the body's position
-  double vx;  // x-component of the body's velocity
-  double vy;  // y-component of the body's velocity
+    double mass;
+    double x;
+    double y;
+    double z;
+    double vx;
+    double vy;
+    double vz;
 } Body;
 
-// function to calculate the acceleration of a body due to the gravitational forces of all other bodies
-void calc_acceleration(Body *bodies, int n, int i, double *ax, double *ay) {
-  *ax = 0;
-  *ay = 0;
-  for (int j = 0; j < n; j++) {
-    if (i == j) {
-      continue;
+
+void cleanup(Body **bodies, int num_bodies) {
+    for(int i = 0; i < num_bodies; i++) {
+        free(bodies[i]);
     }
-    double dx = bodies[j].x - bodies[i].x;
-    double dy = bodies[j].y - bodies[i].y;
-    double r2 = dx * dx + dy * dy;
-    double r = sqrt(r2);
-    double F = G * bodies[i].mass * bodies[j].mass / r2;
-    *ax += F * dx / r;
-    *ay += F * dy / r;
-  }
 }
 
-int main() {
-  // create an array of celestial bodies
-  int n = 3;
-  Body bodies[3] = {
-    {1.0, 0.0, 0.0, 0.0, 0.0},  // sun
-    {1.0e-3, 1.0, 0.0, 0.0, 2 * M_PI},  // earth
-    {2.0e-3, 0.0, 1.0, -2 * M_PI, 0.0}  // moon
-  };
 
-  // simulate the motion of the bodies
-  for (int t = 0; t < MAX_ITERATIONS; t++) {
-    for (int i = 0; i < n; i++) {
-      double ax, ay;
-      calc_acceleration(bodies, n, i, &ax, &ay);
-      bodies[i].vx += ax * DT;
-      bodies[i].vy += ay * DT;
-      bodies[i].x += bodies[i].vx * DT;
-      bodies[i].y += bodies[i].vy * DT;
+Body **generate_initial_population(int n, double max_mass, double max_pos, double max_vel) {
+    Body **bodies = malloc(sizeof(Body *) * n);
+
+    for(int i = 0; i < n; i++) {
+        bodies[i] = malloc(sizeof(Body));
+        bodies[i]->mass = (double)rand() / RAND_MAX * max_mass;
+        bodies[i]->x = (double)rand() / RAND_MAX * max_pos;
+        bodies[i]->y = (double)rand() / RAND_MAX * max_pos;
+        bodies[i]->z = (double)rand() / RAND_MAX * max_pos;
+        bodies[i]->vx = (double)rand() / RAND_MAX * max_vel;
+        bodies[i]->vy = (double)rand() / RAND_MAX * max_vel;
+        bodies[i]->vz = (double)rand() / RAND_MAX * max_vel;
     }
-  }
+    return bodies;
+}
 
-  // print the final positions of the bodies
-  for (int i = 0; i < n; i++) {
-    printf("Body %d: (%.2f, %.2f)\n", i, bodies[i].x, bodies[i].y);
-  }
+void print_boddies(Body **bodies, int n, int iteration) {
+    if (iteration == 0)
+        printf("iteration,mass,x,y,z,vx,vy,vz\n");
+    for(int i = 0; i < n; i++) 
+        printf("%d,%f,%f,%f,%f,%f,%f,%f\n", iteration, bodies[i]->mass, bodies[i]->x, bodies[i]->y, bodies[i]->z, bodies[i]->vx, bodies[i]->vy, bodies[i]->vz);
+}
 
-  return 0;
+
+Body **calculate_iteration(Body** bodies, int num_bodies) {
+    Body** new_bodies = (Body**) malloc(num_bodies * sizeof(Body*));
+    for (int i = 0; i < num_bodies; i++) {
+        new_bodies[i] = malloc(sizeof(Body));
+        double forceX = 0;
+        double forceY = 0;
+        double forceZ = 0;
+        for (int j = 0; j < num_bodies; j++) {
+            double rx = bodies[i]->x - bodies[j]->x;
+            double ry = bodies[i]->y - bodies[j]->y;
+            double rz = bodies[i]->z - bodies[j]->z;
+            double distance = sqrt(pow(rx, 2) + pow(ry, 2) + pow(rz, 2));
+
+            forceX += bodies[j]->mass * rx / (pow(distance, 3) + G);// G je epsilon xd
+            forceY += bodies[j]->mass * ry / (pow(distance, 3) + G); // G je epsilon xd
+            forceZ += bodies[j]->mass * rz / (pow(distance, 3) + G); // G je epsilon xd
+        }      
+
+        forceX *= G * bodies[i]->mass;  
+        forceY *= G * bodies[i]->mass;  
+        forceZ *= G * bodies[i]->mass;  
+
+        double ax = forceX / bodies[i]->mass;
+        double ay = forceY / bodies[i]->mass;
+        double az = forceZ / bodies[i]->mass;
+
+        new_bodies[i]->mass = bodies[i]->mass;
+        new_bodies[i]->x = bodies[i]->mass + bodies[i]->vx * DT + 0.5 * ax * pow(DT, 2);
+        new_bodies[i]->y = bodies[i]->mass + bodies[i]->vy * DT + 0.5 * ay * pow(DT, 2);
+        new_bodies[i]->z = bodies[i]->mass + bodies[i]->vz * DT + 0.5 * az * pow(DT, 2);
+
+        new_bodies[i]->vx = bodies[i]->vx + ax * DT;
+        new_bodies[i]->vy = bodies[i]->vy + ay * DT;
+        new_bodies[i]->vz = bodies[i]->vz + az * DT;
+
+    }
+
+    return new_bodies;
+}
+
+
+int main() {
+    Body **bodies;
+    bodies = generate_initial_population(100, 100, 100, 100);
+    for(int i = 0; i < 100; i ++) {
+        bodies = calculate_iteration(bodies, 100);
+        print_boddies(bodies,100, i);
+    }
+    return 0;
 }
